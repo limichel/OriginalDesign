5e4cf6ce8140509408023b1cadb6dc98d83812a0
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-08-22 11:15:58 -0700

    Edited OriginalDesign.pde and index.html

--- OriginalDesign.pde
+++ OriginalDesign.pde
@@ -1,9 +1,34 @@
 void setup()
 {
-
+	size(400, 400);
 }
 void draw()
 {
-  ellipse(50,50,50,50);
+	petals();
+    center();
+    leaf();
 }
-
+void petals()
+{
+	noStroke();
+	fill(200, 0, 100);
+	ellipse(195, 150, 50, 50);
+	ellipse(230, 175, 50, 50);
+    ellipse(220, 220, 50, 50);
+    ellipse(180, 220, 50, 50);
+    ellipse(165, 180, 50, 50);
+}
+void center()
+{
+    noStroke();
+    fill(0, 0, 100);
+    ellipse(200, 190, 50, 50);
+}
+void leaf()
+{
+    stroke(0, 80, 0);
+    fill(0, 100, 0);
+    bezier(280, 300, 260, 330, 340, 350, 360, 380);
+    bezier(280, 300, 350, 330, 370, 350, 360, 380);
+    line(295, 320, 340, 350);
+}
--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,13 +16,38 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-
+	size(400, 400);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+	petals();
+    center();
+    leaf();
 }
-
+public void petals()
+{
+	noStroke();
+	fill(200, 0, 100);
+	ellipse(195, 150, 50, 50);
+	ellipse(230, 175, 50, 50);
+    ellipse(220, 220, 50, 50);
+    ellipse(180, 220, 50, 50);
+    ellipse(165, 180, 50, 50);
+}
+public void center()
+{
+    noStroke();
+    fill(0, 0, 100);
+    ellipse(200, 190, 50, 50);
+}
+public void leaf()
+{
+    stroke(0, 80, 0);
+    fill(0, 100, 0);
+    bezier(280, 300, 260, 330, 340, 350, 360, 380);
+    bezier(280, 300, 350, 330, 370, 350, 360, 380);
+    line(295, 320, 340, 350);
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
